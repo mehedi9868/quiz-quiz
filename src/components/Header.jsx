a271e03fa9dd1725602ec4d193e9b333a05bfe3c
mehedi9868 <mehedi9868@gmail.com>
2023-04-15 16:37:25 +0600

    add header banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="w-full mx-auto bg-indigo-100 flex items-center">
+            <div className="w-full p-8">
+                <h2 className="text-2xl font-bold mb-4">Test Your Knowledge!</h2>
+                <p className="text-gray-700 text-justify font-normal text-xl">
+                    Welcome to our quiz website! Test your skills in React, JavaScript,
+                    CSS, and Git with our challenging quizzes. Whether you're a beginner
+                    looking to learn more or an experienced developer looking to test
+                    your knowledge, we have quizzes for you. Take one quiz or take them
+                    all and see how you stack up against your peers. Good luck!
+                </p>
+            </div>
+            <div className="w-full p-8">
+                <img
+                    src="/quiz.jpg" alt="Quiz Image" className="rounded-lg shadow-lg"
+                />
+            </div>
         </div>
     );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TopicDetails from './TopicDetails';
+import Header from './Header';
 
 const Home = () => {
     const [topics, setTopics] = useState([])
@@ -11,6 +12,7 @@ const Home = () => {
     console.log(topics);
     return (
         <div className='w-3/4 mx-auto'>
+            <Header></Header>
             <h2 className='text-center text-2xl font-bold my-10'>Total Topics: {topics.length}</h2>
             <div className='grid grid-cols-2 gap-6 p-6'>
                 {
