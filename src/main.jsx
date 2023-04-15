3ce9d7138ba3f507ecff2ea5db135bd8dd95b566
mehedi9868 <mehedi9868@gmail.com>
2023-04-15 10:59:17 +0600

    layout and router defination

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Main from './layout/Main'
+import Home from './components/Home'
+import Statistics from './components/Statistics'
+import Blog from './components/Blog'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
