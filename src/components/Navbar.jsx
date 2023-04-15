f9645d2e89877ac7b7dcc3979c2e77288e41919b
mehedi9868 <mehedi9868@gmail.com>
2023-04-15 12:27:39 +0600

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className="bg-gray-900">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center">
+                        <NavLink to="/" className="text-white font-semibold text-xl tracking-tight">Quiz Quiz</NavLink>
+                    </div>
+                    <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                            <NavLink to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</NavLink>
+                            <NavLink to="/statistics" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Statistics</NavLink>
+                            <NavLink to="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
